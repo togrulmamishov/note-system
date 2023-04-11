325a07315596e7d0d7d89bd4a140b8f0af3ce39d
Togrul <togrul511@gmail.com>
2023-04-12 00:18:29 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# togrulmamishov.github.io
---
+++ src/test/java/com/proxyseller/ProxySellerTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proxyseller.proxyseller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxySellerTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
