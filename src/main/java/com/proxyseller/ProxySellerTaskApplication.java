b458a99839566b597a845cff420b508b628dc714
togrulmamishov <85724734+togrulmamishov@users.noreply.github.com>
2023-04-12 14:01:25 +0400

    Rename main class

--- src/main/java/com/proxyseller/ProxySellerTaskApplication.java
+++ src/main/java/com/proxyseller/ProxySellerTaskApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProxySellerTaskApplication {
+public class NoteSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProxySellerTaskApplication.class, args);
